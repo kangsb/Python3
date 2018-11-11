30bb11f14bc0195169b447c4a3215f0578b116bb
Kang Seungbae <kangsb@gmail.com>
2018-11-11 22:49:17 +0900

    add ILS graph

--- qtLogViewer/main.py
+++ qtLogViewer/main.py
@@ -5,15 +5,18 @@ import sys
 import os
 import time
 import qdarkstyle
-from progressThread import *
+
 from process import DataProcess
+from process import EquipmentType
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMainWindow
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QFormLayout
@@ -25,11 +28,13 @@ from PyQt5.QtWidgets import QProgressBar
 from PyQt5.QtGui import QIcon, QPixmap, QFont
 from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtCore import QThread
+from PyQt5.QtCore import pyqtSlot
 
 from bokeh.io import output_file, show
+from bokeh.models.widgets import Panel, Tabs
+from time import sleep
 
-
-class MyApp(QDialog):
+class MyApp(QWidget):
     fileName = None
 
     def __init__(self):
@@ -38,13 +43,8 @@ class MyApp(QDialog):
         self.left = 100
         self.top = 100
         self.width = 400
-        self.height = 480
+        self.height = 300
         self.initUI()
-        self.initProgressBar()
-
-    def initProgressBar(self):
-        self.pgsb = QProgressBar()
-
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -74,9 +74,19 @@ class MyApp(QDialog):
         self.btnFile = QPushButton("Choose", self)
         self.btnFile.clicked.connect(self.btnFile_clicked)
 
-        lbFile = QLabel("File:", self)
-        lbSep = QLabel("Seperator:", self)
-        lbDec = QLabel("Decimal:", self)
+        equipLayout = QHBoxLayout()
+        self.rbLoc = QRadioButton("LOC")
+        self.rbLoc.setChecked(True)
+        self.equipment = EquipmentType.LOC
+        self.rbLoc.toggled.connect(lambda:self.radioState(self.rbLoc))
+        self.rbGp = QRadioButton("GP")
+        self.rbGp.toggled.connect(lambda:self.radioState(self.rbGp))
+        self.rbDme = QRadioButton("DME")
+        self.rbDme.toggled.connect(lambda:self.radioState(self.rbDme))
+        equipLayout.addWidget(self.rbLoc)
+        equipLayout.addWidget(self.rbGp)
+        equipLayout.addWidget(self.rbDme)
+
         self.leSep = QLineEdit()
         self.leSep.setFont(fontText)
         self.leSep.setPlaceholderText("Delimiter to use. Default ,(comma)")
@@ -104,16 +114,38 @@ class MyApp(QDialog):
         fileOpenLayout.addWidget(self.leFile)
         fileOpenLayout.addWidget(self.btnFile)
 
+        self.cbDateTimeFormat = QComboBox()
+        self.cbDateTimeFormat.addItem('%d.%m.%Y %H:%M:%S')
+        self.cbDateTimeFormat.addItem('%Y-%m-%d %H.%M.%S.%f')
+        self.cbDateTimeFormat.setEditable(True)
+#        self.cbDateTimeFormat.setPlaceholderText('date/time format string')
+        self.cbDateTimeFormat.setCurrentIndex(-1)
+        self.cbDateTimeFormat.currentTextChanged.connect(self.cbTextChanged)  # 현재 인덱스의 데이터가 바뀔 때
+
         propLayout = QGridLayout()
         propLayout.setAlignment(Qt.AlignCenter)
         propLayout.setSpacing(10)
         propLayout.setContentsMargins(0, 0, 0, 0)
-        propLayout.addWidget(lbFile, 0, 0)
-        propLayout.addWidget(lbSep, 1, 0)
-        propLayout.addWidget(lbDec, 2, 0)
-        propLayout.addLayout(fileOpenLayout, 0, 1)
-        propLayout.addWidget(self.leSep, 1, 1)
-        propLayout.addWidget(self.leDecimal, 2, 1)
+
+        propLayout.addWidget(QLabel('Equipment:', self), 0, 0)
+        propLayout.addWidget(QLabel('File:', self), 1, 0)
+        propLayout.addWidget(QLabel('Seperator:', self), 2, 0)
+        propLayout.addWidget(QLabel('Decimal:', self), 3, 0)
+        propLayout.addWidget(QLabel('Date/Time format:', self), 4, 0)
+        propLayout.addLayout(equipLayout, 0, 1)
+        propLayout.addLayout(fileOpenLayout, 1, 1)
+        propLayout.addWidget(self.leSep, 2, 1)
+        propLayout.addWidget(self.leDecimal, 3, 1)
+        propLayout.addWidget(self.cbDateTimeFormat, 4, 1)
+
+
+        self.pbar = QProgressBar(self)
+#        self.pbar.setGeometry(50, 10, 400, 5)
+        self.pbar.setMinimum(0)
+        self.pbar.setMaximum(0)
+        self.pbar.setHidden(True)
+        self.lbStatus = QLabel()
+        self.lbStatus.setText('Ready')
 
         mainLayout = QVBoxLayout()
         mainLayout.setSpacing(10)
@@ -123,19 +155,36 @@ class MyApp(QDialog):
         mainLayout.addLayout(propLayout)
         mainLayout.addWidget(self.btnOk)
         mainLayout.addWidget(self.btnQuit)
+        mainLayout.addWidget(self.pbar)
+        mainLayout.addWidget(self.lbStatus)
+
         self.setLayout(mainLayout)
-        self.adjustSize()
+
+        self.setTabOrder(self.rbLoc, self.rbGp)
+        self.setTabOrder(self.rbGp, self.rbDme)
+        self.setTabOrder(self.rbDme, self.btnFile)
+        self.setTabOrder(self.btnFile, self.leSep)
+        self.setTabOrder(self.leSep, self.leDecimal)
+        self.setTabOrder(self.leDecimal, self.cbDateTimeFormat)
+        self.setTabOrder(self.cbDateTimeFormat, self.btnOk)
+        self.setTabOrder(self.btnOk, self.btnQuit)
+
+#        self.adjustSize()
         self.show()
 
-#    def focusInEvent(self, ev):
-#        self.select_on_mouse_press = time.time()
-#        QLineEdit.focusInEvent(self, ev)
+    def cbTextChanged(self):
+        pass
 
-#    def mousePressEvent(self, ev):
-#        self.leFile.mousePressEvent(self, ev)
-#        if self.select_on_mouse_press is not None and abs(time.time() - self.select_on_mouse_press) < 0.2:
-#        QMessageBox.about(self, "message", "clicked")
-#        self.select_on_mouse_press = None
+    def radioState(self, b):
+        if b.text() == "LOC":
+            if b.isChecked() == True:
+                self.equipment = EquipmentType.LOC
+        elif b.text() == "GP":
+            if b.isChecked() == True:
+                self.equipment = EquipmentType.GP
+        elif b.text() == "DME":
+            if b.isChecked() == True:
+                self.equipment = EquipmentType.DME
 
     def btnFile_clicked(self):
         options = QFileDialog.Options()
@@ -143,33 +192,174 @@ class MyApp(QDialog):
         openFileName = QFileDialog.getOpenFileName(self, 'Open file', "", "All Files (*);;csv File (*.csv);;xlsx File (*.xlsx)", options=options)
         if openFileName != ('', ''):
             _, ext = os.path.splitext(openFileName[0])
+            self.baseName = os.path.basename(openFileName[0])
             if ext.lower() != '.csv' and ext.lower() != '.xlsx':
                 QMessageBox.warning(self, "warning", "File extension error")
                 self.fileName = None
             else:
                 self.fileName = openFileName[0]
                 self.leFile.setText(openFileName[0])
+                self.setWindowTitle(self.baseName)
  
     def btnOk_clicked(self):
         if self.fileName == None:
             QMessageBox.warning(self, "warning", "Please select a file")
         else:
-            sep = self.leSep.text()
-            sep = sep.replace(" ", "") 
-            dec = self.leDecimal.text()
-            dec = dec.replace(" ", "") 
+            self.mainProcess()
 
-            sep = ',' if len(sep) == 0 else sep
-            dec = '.' if len(dec) == 0 else dec
-            print('sec = ' + sep + ', dec = ' + dec)
-            dp = DataProcess(filename=self.fileName, sep=sep, dec=dec, quot='"')
-            print('Program start...')
-            dp.fileOpen()
-            print('File opened.')
-            tabs = dp.makeTabWidget()
-            print('Tab generation finished.')
-            show(tabs)
+    def mainProcess(self):
+        sep = self.leSep.text()
+        sep = sep.replace(" ", "") 
+        dec = self.leDecimal.text()
+        dec = dec.replace(" ", "") 
 
+        sep = ',' if len(sep) == 0 else sep
+        dec = '.' if len(dec) == 0 else dec
+        datetime = self.cbDateTimeFormat.currentText()
+        datetime = None if len(datetime) == 0 else datetime
+        self.dp = DataProcess(filename=self.fileName, sep=sep, dec=dec, quot='"', datetime=datetime)
+        fn = self.baseName.split('.')
+        outfile = fn[0] + '.html'
+        output_file(outfile, title=outfile)
+
+        self.lbStatus.setText('Opening file...')
+        self.dp.fileOpen()
+        if self.equipment == EquipmentType.LOC:
+            self.makeLGTabbedHtml(self.equipment)
+        elif self.equipment == EquipmentType.GP:
+            self.makeLGTabbedHtml(self.equipment)
+        elif self.equipment == EquipmentType.DME:
+            self.makeDMETabbedHtml()
+        else:
+            self.lbStatus.setText('Equipment type error')
+            return
+
+        self.lbStatus.setText('Ready')
+
+    def makeLGTabbedHtml(self, equip):
+        __total = 9
+        current = 1
+        title = None
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CL SDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CL_SDM', 'MON2_CL_SDM')
+        tab1 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CL_DDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CL_DDM', 'MON2_CL_DDM')
+        tab2 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CL RF Level"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CL_RFLEVEL', 'MON2_CL_RFLEVEL')
+        tab3 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "DS SDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_DS_SDM', 'MON2_DS_SDM')
+        tab4 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "DS DDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_DS_DDM', 'MON2_DS_DDM')
+        tab5 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "DS RF Level"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_DS_RFLEVEL', 'MON2_DS_RFLEVEL')
+        tab6 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CLR SDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CLR_SDM', 'MON2_CLR_SDM')
+        tab7 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CLR DDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CLR_DDM', 'MON2_CLR_DDM')
+        tab8 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "CLR RF Level"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_CLR_RFLEVEL', 'MON2_CLR_RFLEVEL')
+        tab9 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "NF SDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_NF_SDM', 'MON2_NF_SDM')
+        tab10 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "NF DDM"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_NF_DDM', 'MON2_NF_DDM')
+        tab11 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "NF RF Level"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_NF_RFLEVEL', 'MON2_NF_RFLEVEL')
+        tab12 = Panel(child=layout, title=title)
+
+        self.lbStatus.setText('Tab gernation all finished.\n')
+
+        tabs = Tabs(tabs=[tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12])
+        sleep(0.5)
+        show(tabs)
+
+    def makeDMETabbedHtml(self):
+        __total = 9
+        current = 1
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Rise Time"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_PULSE_RISE_TIME', 'MON2_TX1_PULSE_RISE_TIME', 'MON1_TX2_PULSE_RISE_TIME', 'MON2_TX2_PULSE_RISE_TIME')
+        tab1 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Decay Time"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_PULSE_DECAY_TIME', 'MON2_TX1_PULSE_DECAY_TIME', 'MON1_TX2_PULSE_DECAY_TIME', 'MON2_TX2_PULSE_DECAY_TIME')
+        tab2 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Duration"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_PULSE_DURATION', 'MON2_TX1_PULSE_DURATION', 'MON1_TX2_PULSE_DURATION', 'MON2_TX2_PULSE_DURATION')
+        tab3 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Spacing"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_PULSE_SPACING', 'MON2_TX1_PULSE_SPACING', 'MON1_TX2_PULSE_SPACING', 'MON2_TX2_PULSE_SPACING')
+        tab4 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Delay"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_TIME_DELAY', 'MON2_TX1_TIME_DELAY', 'MON1_TX2_TIME_DELAY', 'MON2_TX2_TIME_DELAY')
+        tab5 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Transmit Rate"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_TRANSMISSION_RATE', 'MON2_TX1_TRANSMISSION_RATE', 'MON1_TX2_TRANSMISSION_RATE', 'MON2_TX2_TRANSMISSION_RATE')
+        tab6 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Efficiency"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_REPLY_EFFICIENCY', 'MON2_TX1_REPLY_EFFICIENCY', 'MON1_TX2_REPLY_EFFICIENCY', 'MON2_TX2_REPLY_EFFICIENCY')
+        tab7 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Ouput Power"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_PEAK_POWER_OUTPUT', 'MON2_TX1_PEAK_POWER_OUTPUT', 'MON1_TX2_PEAK_POWER_OUTPUT', 'MON2_TX2_PEAK_POWER_OUTPUT')
+        tab8 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Create Tab{} ({}/{})...'.format(current, current, __total))
+        current += 1
+        title = "Frequency"
+        layout = self.dp.makeGraph(title, 'TS', 'MON1_TX1_FREQUENCY', 'MON2_TX1_FREQUENCY', 'MON1_TX2_FREQUENCY', 'MON2_TX2_FREQUENCY')
+        tab9 = Panel(child=layout, title=title)
+        self.lbStatus.setText('Tab gernation all finished.\n')
+
+        tabs = Tabs(tabs=[tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9])
+        sleep(0.5)
+        show(tabs)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- qtLogViewer/process.py
+++ qtLogViewer/process.py
@@ -8,10 +8,14 @@ import numpy as np
 from datetime import datetime
 from bokeh.io import output_file, show
 from bokeh.layouts import row
+from bokeh.layouts import column
 from bokeh.plotting import figure
 from bokeh.models import CheckboxGroup, CustomJS
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.models.widgets import Panel, Tabs
+
+from PyQt5.QtCore import QObject, pyqtSignal
+import weakref
 import enum
 
 @enum.unique
@@ -22,36 +26,47 @@ class EquipmentType(enum.Enum):
     VOR = 4
 
 
-class DataProcess:
-    def __init__(self, filename, sep=',', dec='.', quot='"'):
+class DataProcess(QObject):
+    def __init__(self, filename, sep=',', dec='.', quot='"', datetime=None):
+        super().__init__()
         self.sep = sep
         self.dec = dec
         self.quot = quot
         self.filename = filename
+        self.datetime_format = datetime
         self.equipment = EquipmentType.DME
+
+    def printProgress(self, str):
+        pass
 
     def fileOpen(self):
         _, ext = os.path.splitext(self.filename)
         if ext.lower() == '.csv':
-            return self.openCSV()
+            self.df = pd.read_csv(self.filename, sep=self.sep, quotechar=self.quot, decimal=self.dec, low_memory=False)
         elif ext.lower() == '.xlsx':
-            return self.openXlsx()
+            self.df = pd.read_excel(self.filename)
         else:
             print('unknown file')
+            return
+        
+        try:
+            self.bDateTime = True
+#            print(self.datetime_format)
+            self.df['TS'] = self.df['TS'].map(lambda x: datetime.strptime(str(x), self.datetime_format))
+        except:
+            import sys
+            self.bDateTime = False
+            print("Unexpected error:", sys.exc_info()[0])
+            self.df['TS'] = self.df.index
 
-    def openCSV(self):
-        self.df = pd.read_csv(self.filename, sep=self.sep, quotechar=self.quot, decimal=self.dec, low_memory=False, parse_dates=[0])
-        return self.df.columns.tolist()
+    def makeGraph(self, title, ts, *col_list):
+        if self.bDateTime:
+            f = figure(width=1000, height=500, x_axis_type="datetime", title=title)
+        else:
+            f = figure(width=1000, height=500, title=title)
 
-    def openXlsx(self):
-        self.df = pd.read_excel(self.filename)
-        return self.df.columns.tolist()
-
-    def makeGraph(self, ts, *col_list):
-        output_file("output.html", title="example")
-
-        f = figure(width=1000, height=500, x_axis_type="datetime", title=None)
-        props = dict(line_width=2, line_alpha=0.7)
+        props = dict(line_width=1, line_alpha=0.7)
+        
         tim = self.df[ts]
 
         labels  = []
@@ -71,9 +86,14 @@ class DataProcess:
     
         checkbox = CheckboxGroup(labels=labels, active=active, width=300)
         args['checkbox'] = checkbox
-
         checkbox.callback = CustomJS.from_coffeescript(args=args, code=code)
-        layout = row(checkbox, f)
+
+        if self.bDateTime:
+            fActive = figure(width=1000, height=100, x_axis_type="datetime", y_range=(0, 1))
+        else:
+            fActive = figure(width=1000, height=100)
+        fActive.line(tim, self.df['CURRENT_TX'], color='black', legend='Active Tx', **props)
+        layout = column(checkbox, f, fActive)
 
         return layout
 
@@ -88,37 +108,6 @@ class DataProcess:
 #        l1.visible = 1 in checkbox.active;
 #        """)
 
-    def makeTabWidget(self):
-        layout = self.makeGraph('TS', 'MON1_TX1_PULSE_RISE_TIME', 'MON2_TX1_PULSE_RISE_TIME', 'MON1_TX2_PULSE_RISE_TIME', 'MON2_TX2_PULSE_RISE_TIME')
-        tab1 = Panel(child=layout, title="Rise Time")
-        print('Tab1 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_PULSE_DECAY_TIME', 'MON2_TX1_PULSE_DECAY_TIME', 'MON1_TX2_PULSE_DECAY_TIME', 'MON2_TX2_PULSE_DECAY_TIME')
-        tab2 = Panel(child=layout, title="Decay Time")
-        print('Tab2 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_PULSE_DURATION', 'MON2_TX1_PULSE_DURATION', 'MON1_TX2_PULSE_DURATION', 'MON2_TX2_PULSE_DURATION')
-        tab3 = Panel(child=layout, title="Duration")
-        print('Tab3 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_PULSE_SPACING', 'MON2_TX1_PULSE_SPACING', 'MON1_TX2_PULSE_SPACING', 'MON2_TX2_PULSE_SPACING')
-        tab4 = Panel(child=layout, title="Spacing")
-        print('Tab4 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_TIME_DELAY', 'MON2_TX1_TIME_DELAY', 'MON1_TX2_TIME_DELAY', 'MON2_TX2_TIME_DELAY')
-        tab5 = Panel(child=layout, title="Delay")
-        print('Tab5 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_TRANSMISSION_RATE', 'MON2_TX1_TRANSMISSION_RATE', 'MON1_TX2_TRANSMISSION_RATE', 'MON2_TX2_TRANSMISSION_RATE')
-        tab6 = Panel(child=layout, title="Transmit Rate")
-        print('Tab6 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_REPLY_EFFICIENCY', 'MON2_TX1_REPLY_EFFICIENCY', 'MON1_TX2_REPLY_EFFICIENCY', 'MON2_TX2_REPLY_EFFICIENCY')
-        tab7 = Panel(child=layout, title="Efficiency")
-        print('Tab7 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_PEAK_POWER_OUTPUT', 'MON2_TX1_PEAK_POWER_OUTPUT', 'MON1_TX2_PEAK_POWER_OUTPUT', 'MON2_TX2_PEAK_POWER_OUTPUT')
-        tab8 = Panel(child=layout, title="Ouput Power")
-        print('Tab8 finished...')
-        layout = self.makeGraph('TS', 'MON1_TX1_FREQUENCY', 'MON2_TX1_FREQUENCY', 'MON1_TX2_FREQUENCY', 'MON2_TX2_FREQUENCY')
-        tab9 = Panel(child=layout, title="Frequency")
-        print('Tab9 finished...')
-
-        tabs = Tabs(tabs=[tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9])
-        return tabs
 
 
 if __name__ == '__main__':
